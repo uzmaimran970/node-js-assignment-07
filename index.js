bf0f35700cfcbc166102064afaaff2861cfe686c
Uzma imran <148060110+uzmaimran970@users.noreply.github.com>
2024-03-08 16:17:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+// 7.Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// addition
+let a = 6;
+let b = 2;
+console.log(a + b);
+// substraction
+let c = 10;
+let d = 2;
+console.log(c - d);
+// multiplication
+let e = 2;
+let f = 4;
+console.log(e * f);
+// division
+let g = 16;
+let h = 2;
+console.log(g / h);
---
+++ index.ts
@@ -0,0 +1,29 @@
+// 7.Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+// addition
+let a: number = 6
+let b: number = 2
+
+console.log (a+b);
+
+// substraction
+
+let c: number = 10
+let d: number = 2
+
+console.log (c-d);
+
+// multiplication
+
+let e: number = 2
+let f: number = 4
+
+console.log (e*f);
+
+// division
+
+let g: number = 16
+let h: number = 2
+
+
+console.log(g/h);
